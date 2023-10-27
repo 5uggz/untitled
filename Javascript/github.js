d55c3cfde88d3935def3fa64705126e67cb56101
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-27 08:22:49 -0400

    Create github.js

---
+++ Javascript/github.js
@@ -0,0 +1,7 @@
+// Redirect the user to a new URL
+function redirectToLink() {
+    window.location.href = "https://www.example.com"; // Replace later with github link
+}
+
+// Call the function to trigger the redirection
+redirectToLink();
